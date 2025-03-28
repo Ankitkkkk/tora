d39e193b84dd606813d1fafdff0e10ad920bc848
zoro <kr.ankit253@gmail.com>
2025-03-29 00:36:26 +0530

    remove boilerplate

---
+++ src/component/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home-box'>
+        <h2>Create Connection +</h2>
+        
+    </div>
+  )
+}
+
+export default Home
